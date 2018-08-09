9a719721aa78d4cd67b3050e2cb1d6c04ac634c9
imaccimm24 <jabautista065>
2018-08-09 15:54:28 -0500

    segundo button

--- app/src/main/java/com/example/imaccimm24/prueba/MainActivity.java
+++ app/src/main/java/com/example/imaccimm24/prueba/MainActivity.java
@@ -2,12 +2,15 @@ package com.example.imaccimm24.prueba;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button btn1, btn2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
 }
